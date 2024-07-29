5ccaf08c16b5eee24bc013fd52eee8aeae438b63
Isabelle Hawkins <ishawkins26@gmail.com>
2024-07-29 17:04:08 +1000

    ** End of Day Week 2: ADV AI **

--- Advanced AI/Assets/ClickDrag.cs
+++ Advanced AI/Assets/ClickDrag.cs
@@ -14,13 +14,54 @@ public class ClickDrag : MonoBehaviour
     //public GameObject marker;
     private void Start()
     {
-
-        bool result =           x == y ? a : b;
         _camera = Camera.main ? Camera.main : FindObjectOfType<Camera>();
     }
 
     private void update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            // Bool value
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                _selectionDistance = hit.distance;
+                -_dragObject = hit.rigidbody;
+
+                _offset = hit.point;
+                    /*_camera.ScreenToViewportPoint(
+                    new vector3(Input.mousePosition.x,
+                    Input.mousePosition.y,
+                    _selectionDistance));*/
+                _originalPosition = hit.collider.transform.position;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            _dragObject = null;
+        }
+
+        if( _dragObject != null )
+        {
+            _selectionDistance += Input.mouseScrollDelta.y * 0.2f;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if(_dragObject)
+        {
+            Vector3 mouseObjectDelta = _camera.ScreenToViewportPoint(
+                    new vector3(Input.mousePosition.x,
+                    Input.mousePosition.y,
+                    _selectionDistance));
+            _dragObject.velocity = (_originalPosition + mouseObjectDelta
+                - _dragObject.transform.position) * forceAmount * Time.deltaTime;
+        }
+    }
+
 }
